ec130ac5ac4643fd324413f47b8cadf4005470d9
Glenn Lagasse <glagasse@glagasse.org>
2017-08-01 19:50:58 -0700

    Refresh emacs configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,23 @@
+(require 'package)
+
+(setq package-enable-at-startup nil) ; tells emacs not to load any packages before starting up
+;; the following lines tell emacs where on the internet to look up
+;; for new packages.
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "https://melpa.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+(package-initialize)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package) ; unless it is already installed
+  (package-refresh-contents) ; updage packages archive
+  (package-install 'use-package)) ; and install the most recent version of use-package
+
+(require 'use-package)
 
 ;; Disable chrome
 (if (display-graphic-p)
@@ -8,79 +28,62 @@
   ;; Make tab key work in tty mode
   (local-set-key [tab] 'tab-to-tab-stop)
 )
-
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
-(require 'package)
-(setq package-enable-at-startup nil)
+(setq user-full-name "Glenn Lagasse"
+      user-mail-address "glagasse@glagasse.org")
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
+(setq delete-old-versions -1 )		; delete excess backup versions silently
+(setq version-control t )		; use version control
+(setq vc-make-backup-files t )		; make backups file even when in version controlled dir
+(setq backup-directory-alist `(("." . "~/.emacs.d/backups")) ) ; which directory to put backups file
+(setq vc-follow-symlinks t )				       ; don't ask for confirmation when opening symlinked file
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)) ) ;transform backups file name
+(setq inhibit-startup-screen t )	; inhibit useless and old-school startup screen
+(setq ring-bell-function 'ignore )	; silent bell when you make a mistake
+(setq coding-system-for-read 'utf-8 )	; use utf-8 by default
+(setq coding-system-for-write 'utf-8 )
+(setq sentence-end-double-space nil)	; sentence SHOULD end with only a point.
+(setq fill-column 80)		; toggle wrapping text at the 80th character
 
-;; (package-refresh-contents)
+;(global-linum-mode)
 
-(package-initialize)
+(use-package general :ensure t)
+(general-define-key
+  ;; replace default keybindings
+  ;;"C-s" 'swiper             ; search for string in current buffer
+  "M-x" 'counsel-M-x        ; replace default M-x with ivy backend
+)
 
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;; Make sure to have downloaded archive description.
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
-(use-package enh-ruby-mode
-  :mode "\\.rb\\'"
-  :config
-    (setq enh-ruby-deep-indent-paren nil) ; Don't indent ruby function parameters at column index of function parentheses
-    )
-
-(use-package ruby-end)
-
-(use-package robe
+(use-package which-key :ensure t
   :init
-  (add-hook 'enh-ruby-mode-hook 'robe-mode)
-  :config
-  (push 'company-robe company-backends))
+  (setq which-key-idle-delay 0.5)
+  (which-key-mode)
+  :diminish which-key-mode
+)
 
-(use-package company
+(use-package ivy
   :ensure t
-  :defer t
-  :init
-  (global-company-mode)
+  :diminish (ivy-mode . "") ; does not display ivy in the modeline
+  :init (ivy-mode 1)        ; enable ivy globally at startup
+  :bind (:map ivy-mode-map  ; bind in the ivy buffer
+         ("C-'" . ivy-avy)) ; C-' to ivy-avy
   :config
-  (setq company-idle-delay 0.2)
-  (setq company-selection-wrap-around t)
-  (define-key company-active-map [tab] 'company-complete)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous))
+  (setq ivy-use-virtual-buffers t)   ; extend searching to bookmarks and …
+  (setq ivy-height 20)               ; set height of the ivy window
+  (setq ivy-count-format "(%d/%d) ") ; count format, from the ivy help page
+  )
 
-(use-package helm
-  :ensure t
-  :diminish helm-mode
-  :config
-  (helm-mode 1)
-  (setq helm-buffers-fuzzy-matching t)
-  (setq helm-autoresize-mode t)
-  (setq helm-buffer-max-length 40)
-  (global-set-key (kbd "M-x") 'helm-M-x)
-  (global-set-key (kbd "C-x C-f") 'helm-find-files)
-  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
-  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
-  (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-  (define-key helm-find-files-map (kbd "<C-backspace>") #'backward-kill-word)
-  (define-key helm-map (kbd "S-SPC") 'helm-toggle-visible-mark)
-  (define-key helm-find-files-map (kbd "C-k") 'helm-find-files-up-one-level))
-
-(use-package elpy
-  ;; pip install --user rope jedi flake8 importmagic autopep8 yapf
-  :ensure t
-  :config
-  (progn
-    (elpy-enable)))
+(use-package counsel :ensure t
+  :bind*                           ; load counsel when pressed
+  (("M-x"     . counsel-M-x)       ; M-x use counsel
+   ("C-x C-f" . counsel-find-file) ; C-x C-f use counsel-find-file
+   ("C-x C-r" . counsel-recentf))   ; search recently edited files
+;   ("C-c f"   . counsel-git)       ; search for files in git repo
+;   ("C-c s"   . counsel-git-grep)  ; search for regexp in git repo
+;   ("C-c /"   . counsel-ag)        ; search for regexp in git repo using ag
+;   ("C-c l"   . counsel-locate))   ; search for files or else using locate
+  )
 
 (use-package evil
   :ensure t
@@ -92,14 +95,29 @@
 (use-package magit
   :ensure t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("316d29f8cd6ca980bf2e3f1c44d3a64c1a20ac5f825a167f76e5c619b4e92ff4" default))))
+(use-package enh-ruby-mode :ensure t
+  :mode
+  (("Capfile" . enh-ruby-mode)
+   ("Gemfile\\'" . enh-ruby-mode)
+   ("Rakefile" . enh-ruby-mode)
+   ("Vagrantfile" . enh-ruby-mode)
+   ("\\.rb" . enh-ruby-mode)
+   ("\\.ru" . enh-ruby-mode))
+  :init
+  (rename-modeline "enh-ruby-mode" enh-ruby-mode "Ruby")
+)
+
+(use-package smart-mode-line
+  :ensure t
+  :init
+  (smart-mode-line-enable)
+  :diminish
+  :config
+  (progn
+    (setq sml/no-confirm-load-theme t)
+    (sml/setup)
+  )
+)
 
 (use-package zenburn-theme
   :ensure t
@@ -114,12 +132,41 @@
 (use-package vimrc-mode
   :ensure t)
 
-;; Configure backups behaviour
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
-(setq delete-old-versions -1)
-(setq version-control t)
-(setq vc-make-backup-files t)
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+(use-package rainbow-delimiters
+  :ensure t
+  :diminish
+  :defer t
+  :config
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+)
+
+(use-package flycheck
+  :ensure t
+  :defer t
+  :diminish
+  :config
+  (add-hook 'prog-mode-hook (lambda () (flycheck-mode)))
+)
+
+(use-package flycheck-color-mode-line
+    :ensure t
+    :config
+    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
+)
+
+(use-package yaml-mode
+  :ensure t
+  :defer t
+  :diminish
+)
+
+(use-package git-gutter+
+  :ensure t
+  :diminish
+  :defer t
+  :config
+  (global-git-gutter+-mode)
+)
 
 ;; Display day/time/date
 (setq display-time-day-and-date t)
@@ -127,9 +174,6 @@
 
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-echo-area-message "glagasse")
-
-;; Sentences end with a single space
-(setq sentence-end-double-space nil)
 
 ;; Change "yes or no" to "y or n"
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -139,14 +183,15 @@
 
 (column-number-mode 1)
 (show-paren-mode 1)
+(setq show-paren-delay 0)
 
-(if (eq system-type 'darwin) ;; mac specific settings
-    (progn
-      (setq mac-option-modifier 'alt)
-      (setq mac-command-modifier 'meta)
-      ;;(global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
-      ))
-
+;; key bindings
+(when (eq system-type 'darwin) ;; mac specific settings
+  (setq mac-option-modifier 'alt)
+  (setq mac-command-modifier 'meta)
+  (custom-set-variables '(epg-gpg-program  "/usr/local/bin/gpg2"))
+)
+  
 (require 'cl)
 (defun font-candidate (&rest fonts)
   "Return existing font which first match."
@@ -171,7 +216,7 @@
   "Figure out what font size to use"
   (cond ((eq 1080 screen-pixel-height) "14") ;; Dell Work Monitor
         ((eq 1440 screen-pixel-height) "14") ;; Dell U2713HM
-        ((eq 900 screen-pixel-height) "16") ;; Macbook Air
+        ((eq 900 screen-pixel-height) "18") ;; Macbook Air
         ((eq 600 screen-pixel-height) "12") ;; Acer Aspire One
         (t "12")))
 
@@ -182,7 +227,3 @@
 
 (set-face-attribute 'default nil :font gl/my-font-spec)
 (set-frame-font gl/my-font-spec)
-
-;; It would be good to put this in the use-package declarartion for zenburn-theme
-(custom-set-faces
- '(font-lock-comment-face ((t (:foreground "#7F9F7F" :slant italic :family gl/my-font-spec)))))
