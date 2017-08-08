ec38cc5994e754a743efabaf08407ea3194de4de
Glenn Lagasse <glagasse@glagasse.org>
2017-08-08 11:42:24 -0700

    Add better scrolling settings
    Change-Id: I4e5c4588b877a277f397f4f946b09e296abe67fc

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,6 +62,12 @@
 (global-auto-revert-mode t)             ; automatically reload buffer from disk when changed
 (global-linum-mode)
 
+;; Better scroll settings (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+
 ;; Make files that start with !# executable on save
 (add-hook 'after-save-hook
           #'executable-make-buffer-file-executable-if-script-p)
